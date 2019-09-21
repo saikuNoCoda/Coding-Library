b679ab990ecf6e6a82507b5efa57b433f2b442e2
failedcoder12 <divyanshverma12@gmail.com>
2019-09-21 14:05:59 +0530

    ZALGO

--- basis_of_vector.cpp
+++ basis_of_vector.cpp
@@ -3,16 +3,17 @@
 // GAUSS ELEMINATION VARIENT
 // COPIED FROM https://codeforces.com/blog/entry/68953
 // MORE QUESTION AT https://codeforces.com/blog/entry/68953
+// https://www.codechef.com/viewsolution/26693249
 
 const ll MAXN = 1e5 + 10;
-const ll LOG_A = 20;
+const ll LOG_A = 62;
 
 ll basis[LOG_A];
 ll sz;
 
 void insertVector(ll mask){
-    for(ll i=0;i<LOG_A;i++){
-        if((mask&(1<<i)) == 0) continue;
+    for(ll i=LOG_A;i>=0;i--){
+        if((mask&(1LL<<i)) == 0) continue;
 
         if(!basis[i]){
             basis[i] = mask;
---
+++ z-algorithm.cpp
@@ -0,0 +1,15 @@
+
+const ll MAXN = 1e5 + 10;
+ll z[MAXN];
+
+void z_function(string s){
+    ll n = s.size();
+ 
+    FOR(i,0,n+1) z[i] = 0;
+ 
+    for(ll i=1,l=0,r=0;i<n;i++){
+        if(i<=r) z[i] = min(r-i+1,z[i-l]);
+        while(i+z[i]<n && s[z[i]] == s[i+z[i]]) z[i]++;
+        if(i+z[i]-1>r) l=i,r=i+z[i]-1;
+    }
+}
